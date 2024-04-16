ef87f34a87a7c8a94100c1da083ae92803f74526
SofianykDasha <91177584+SofianykDasha@users.noreply.github.com>
2024-04-16 22:46:48 +0300

    Update script.js

--- lab4/script.js
+++ lab4/script.js
@@ -6,19 +6,20 @@ const SortingLibrary = {
 
         for (let i = 0; i < arr.length - 1; i++) {
             for (let j = i + 1; j < arr.length; j++) {
-                if (arr[i] !== undefined && arr[j] !== undefined)
-                if (arr[i] > arr[j] && sortOrder === 'asc') {
-                    let temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    swaps++;
-                } else if (arr[i] < arr[j] && sortOrder === 'desc') {
-                    let temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    swaps++;
+                if (arr[i] !== undefined && arr[j] !== undefined) {
+                    if (arr[i] > arr[j] && sortOrder === 'asc') {
+                        let temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        swaps++;
+                    } else if (arr[i] < arr[j] && sortOrder === 'desc') {
+                        let temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        swaps++;
+                    }
+                    comparisons++;
                 }
-                comparisons++;
             }
         }
 
