93b274e4382f94a1a37698d1c9ebf0d3055ffec3
SofianykDasha <91177584+SofianykDasha@users.noreply.github.com>
2024-04-16 23:01:52 +0300

    Update script.js

--- lab4/script.js
+++ lab4/script.js
@@ -62,12 +62,14 @@ const SortingLibrary = {
         for (let i = 1; i < arr.length; i++) {
             let current = arr[i];
             let j = i - 1;
-            while (j >= 0 && ((arr[j] > current && sortOrder === 'asc') || (arr[j] < current && sortOrder === 'desc'))) {
+            while (j >= 0 && ((arr[j] !== undefined && arr[j] > current && sortOrder === 'asc') || (arr[j] !== undefined && arr[j] < current && sortOrder === 'desc'))) {
                 if (arr[i] !== undefined && arr[j] !== undefined) {
                     arr[j + 1] = arr[j];
                     j--;
                     swaps++;
                     comparisons++;
+                } else {
+                    j--;
                 }
             }
             arr[j + 1] = current;
