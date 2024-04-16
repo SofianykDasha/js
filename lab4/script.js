06ad166992fb07dd85bb26830265160874306198
SofianykDasha <91177584+SofianykDasha@users.noreply.github.com>
2024-04-16 23:27:06 +0300

    Update script.js

--- lab4/script.js
+++ lab4/script.js
@@ -78,17 +78,17 @@ const SortingLibrary = {
 
     // Метод сортування Шелла
     shellSort: function(arr, sortOrder = 'asc') {
-        for (let interval = n / 2; interval > 0; interval /= 2) {
-        //Sort the element using insertion sort in each cycle.
-        for (let i = interval; i < n; i += 1) {
-          let temp = arr[i];
-          let j;
-          for (j = i; j >= interval && arr[j - interval] > temp; j -= interval) {
-            arr[j] = arr[j - interval];
+        for (let interval = arr.length / 2; interval > 0; interval /= 2) {
+            //Sort the element using insertion sort in each cycle.
+            for (let i = interval; i < n; i += 1) {
+              let temp = arr[i];
+              let j;
+              for (j = i; j >= interval && arr[j - interval] > temp; j -= interval) {
+                arr[j] = arr[j - interval];
+              }
+              arr[j] = temp;
+            }
           }
-          arr[j] = temp;
-        }
-      }
     
         console.log(`Comparisons: ${comparisons}, Swaps: ${swaps}`);
         return arr;
