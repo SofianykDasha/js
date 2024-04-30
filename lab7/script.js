4a9b3bcf1fa4158a42d46b9d6d245cca42e7794f
SofianykDasha <91177584+SofianykDasha@users.noreply.github.com>
2024-04-30 22:01:15 +0300

    Add files via upload

---
+++ lab7/script.js
@@ -0,0 +1,39 @@
+function loadCatalog() {
+  fetch('http://localhost:3000/catalog')
+      .then(response => response.json())
+      .then(data => {
+          let categories = '';
+          data.forEach(category => {
+              categories += `<a href="#" onclick="loadCategory('${category.shortname}')">${category.name}</a><br>`;
+          });
+          
+          let specialCategoryIndex = Math.floor(Math.random() * 3) + 0;
+          console.log(specialCategoryIndex);
+          categories += `<a href="#" onclick="loadCategory('${data[specialCategoryIndex].shortname}', '${data[specialCategoryIndex].name}')">Specials</a><br>`;
+          document.getElementById('menu').innerHTML = categories;
+      });
+}
+
+function loadCategory(shortname, name) {
+  fetch(`http://localhost:3000/${shortname}`)
+      .then(response => response.json())
+      .then(data => {
+          let categoryContent = `<h3>${data.name}</h3>`;
+          data.items.forEach(item => {
+              categoryContent += `
+                  <div class="card">
+                      <img src="https://via.placeholder.com/50" class="card-img-top" alt="${item.name}">
+                      <div class="card-body">
+                          <h5 class="card-title">${item.name}</h5>
+                          <p class="card-text">${item.description}</p>
+                          <p class="card-text">Price: ${item.price}</p>
+                      </div>
+                  </div>
+              `;
+          });
+          document.getElementById('content').innerHTML = `<h1>${name}</h1>`;
+          document.getElementById('content').innerHTML = categoryContent;
+      });
+}
+
+document.addEventListener('DOMContentLoaded', loadCatalog);
